242251e37f63867dffc8098b0d226d1620105041
Aric Gilinsky <aric.gilinsky@gmail.com>
2023-07-01 16:37:11 +0100

    added router

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,18 @@ import Footer from './components/Footer';
 import './App.css';
 import { useState } from 'react';
 import Body from './components/Body';
-
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 
 function App() {
 
   return (
     <div className="App">
-      <Header />
-      <Body/>
+            <Header />
+      <Router>
+      <Routes>
+        <Route path="DylanSwift/" element={<Body />} />
+      </Routes>
+      </Router>
       <Footer />
     </div>
   );
