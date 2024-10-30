7b4168ba2b83cb59ddd79123b3fe4aa8a021387d
ASGili <127898109+ASGili@users.noreply.github.com>
2024-10-30 21:57:50 +0000

    Update Footer.tsx

--- src/containers/Footer.tsx
+++ src/containers/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = ()=> {
 
     return (      
     <footer className="app-footer">
-    <p>Aric Gilinsky ©2023</p>
+    <p>Aric Gilinsky</p>
     <ul className="header-footer-links">
         <a className="header-footer-buttons" href="http://asgili.github.io">About Me</a>
         <a className="header-footer-buttons" href="http://github.com/ASGili">My GitHub</a>
